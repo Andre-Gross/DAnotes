6343967d350302dbe3acb24b84816bba120937a8
Andre-Gross <andre.handy94@gmail.com>
2025-10-12 14:55:26 +0200

    feat: Add NoteListService for Firestore collections
    - Create NoteListService with dependency injection
    - Add getNotesRef() method to access notes collection
    - Add getTrashRef() method to access trash collection
    - Configure service as singleton (providedIn: 'root')

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+    firestore: Firestore = inject(Firestore);
+
+
+    constructor() { }
+
+
+    // itemCollection = collection(this.firestore, 'items');
+
+    getNotesRef() {
+        return collection(this.firestore, 'notes')
+    }
+
+
+    getTrashRef() {
+        return collection(this.firestore, 'trash')
+    }
+
+
+    getSingleDocRef(colId:string, docId:string) {
+        return doc(collection(this.firestore, docId))
+    }
 }
