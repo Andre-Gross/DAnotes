01a923e7d455d29947e86cea7f8a3c26144e51d7
Andre-Gross <andre.handy94@gmail.com>
2025-10-21 16:12:00 +0200

    feat(note-list): fetch data from server instead of dummy data

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,16 +1,61 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
+import { collectionData, Firestore, collection, doc, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
     providedIn: 'root'
 })
 export class NoteListService {
+    normalNotes: Note[] = [];
+    thrashNotes: Note[] = [];
+
+    unsubNotes;
+    unsubTrash;
 
     firestore: Firestore = inject(Firestore);
 
 
-    constructor() { }
+    constructor() {
+        this.unsubNotes = this.subNotesList();
+        this.unsubTrash = this.subTrashList();
+    }
 
+
+    ngOnDestroy() {
+        this.unsubNotes();
+        this.unsubTrash();
+    }
+
+
+    subTrashList() {
+        return onSnapshot(this.getTrashRef(), (list) => {
+            this.thrashNotes = [];
+            list.forEach((element) => {
+                this.thrashNotes.push(this.setNoteObject(element.data(), element.id));
+            })
+        });
+    }
+
+
+    subNotesList() {
+        return onSnapshot(this.getNotesRef(), (list) => {
+            this.normalNotes = [];
+            list.forEach((element) => {
+                this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+            })
+        });
+    }
+
+
+    setNoteObject(obj: any, id: string): Note {
+        return {
+            id: id || "",
+            type: obj.type || "note",
+            title: obj.title || "",
+            content: obj.content || "",
+            marked: obj.marked || false,
+        }
+    }
 
     // itemCollection = collection(this.firestore, 'items');
 
@@ -24,7 +69,7 @@ export class NoteListService {
     }
 
 
-    getSingleDocRef(colId:string, docId:string) {
+    getSingleDocRef(colId: string, docId: string) {
         return doc(collection(this.firestore, docId))
     }
 }
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -8,74 +8,37 @@ import { NoteComponent } from './note/note.component';
 
 
 @Component({
-  selector: 'app-note-list',
-  standalone: true,
-  imports: [FormsModule, CommonModule, NoteComponent],
-  templateUrl: './note-list.component.html',
-  styleUrl: './note-list.component.scss'
+    selector: 'app-note-list',
+    standalone: true,
+    imports: [FormsModule, CommonModule, NoteComponent],
+    templateUrl: './note-list.component.html',
+    styleUrl: './note-list.component.scss'
 })
 export class NoteListComponent {
-  noteList: Note[] = [];
-  favFilter: "all" | "fav" = "all";
-  status: "notes" | "trash" = "notes";
+    noteList: Note[] = [];
+    favFilter: "all" | "fav" = "all";
+    status: "notes" | "trash" = "notes";
 
-  constructor(public noteService: NoteListService) {
-    this.noteList = this.getDummyData()
-  }
+    constructor(private noteService: NoteListService) {}
 
-  changeFavFilter(filter:"all" | "fav"){
-    this.favFilter = filter;
-  }
 
-  changeTrashStatus(){
-    if(this.status == "trash"){
-      this.status = "notes";
-    } else {
-      this.status = "trash";
-      this.favFilter = "all";
+    getList(type: 'normal' | 'trash' = 'normal'): Note[] {
+        return type === 'normal'
+            ? this.noteService.normalNotes
+            : this.noteService.thrashNotes;
     }
-  }
 
 
+    changeFavFilter(filter: "all" | "fav") {
+        this.favFilter = filter;
+    }
 
-
-  getDummyData(): Note[] {
-    return [
-      {
-        id: "21sasd561dd4sdf",
-        type: "note",
-        title: "Block, Inline, and Inline-Block",
-        content: "https://www.youtube.com/watch?v=x_i2gga-sYg",
-        marked: true,
-      },
-      {
-        id: "25sd4f561w54sdf",
-        type: "note",
-        title: "css selector",
-        content: `kind p > b   (direktes kind) 
-        nachfahren p b  (alle nachfahren)
-        geschwister p ~ b (auf gleicher ebene ist VOR dem p ein b)`,
-        marked: true,
-      },
-      {
-        id: "54a4s6d546ff",
-        type: "note",
-        title: "aufräumen",
-        content: "Wohnzimmer saugen",
-        marked: false,
-      },
-      {
-        id: "2a35s4d654a6s4d",
-        type: "note",
-        title: "links",
-        content: `Reihenfolge: a:visited 
-        a:focus 
-        a:hover 
-        a:active
-        merkspruch: LoVe HAte`,
-        marked: true,
-      }
-    ];
-  }
-
+    changeTrashStatus() {
+        if (this.status == "trash") {
+            this.status = "notes";
+        } else {
+            this.status = "trash";
+            this.favFilter = "all";
+        }
+    }
 }
